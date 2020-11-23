f40f21ead3fdf09bb261f1a6c6a7434f25660e23
Itzdvbravo <47894997+Itzdvbravo@users.noreply.github.com>
2020-11-23 17:58:11 +0530

    Minecraft 1.16.100 Update.

--- src/Itzdvbravo/Unique/Main.php
+++ src/Itzdvbravo/Unique/Main.php
@@ -3,34 +3,38 @@
 namespace Itzdvbravo\Unique;
 
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerCreationEvent;
-use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\server\DataPacketSendEvent;
 use pocketmine\item\Item;
 use pocketmine\item\ItemFactory;
+use pocketmine\network\mcpe\convert\ItemTranslator;
 use pocketmine\network\mcpe\protocol\StartGamePacket;
-use pocketmine\network\mcpe\protocol\types\DimensionIds;
-use pocketmine\network\mcpe\protocol\types\SpawnSettings;
-use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
-use pocketmine\Server;
-use pocketmine\utils\Config;
-use const pocketmine\RESOURCE_PATH;
+use ReflectionObject;
 
 class Main extends PluginBase implements Listener {
 
 	public function onEnable() : void{
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->saveResource("config.json");
-        	ItemFactory::registerItem(new Item(CustomiesBravo::GEM, 0, "Gem"));
+
+        ItemFactory::registerItem(new Item(CustomiesBravo::GEM, 0, "Gem"));
+        CustomiesBravo::init($this);
+
+
+        $instance = ItemTranslator::getInstance();
+        $ref = new ReflectionObject($instance);
+        $r1 = $ref->getProperty("simpleCoreToNetMapping");
+        $r2 = $ref->getProperty("simpleNetToCoreMapping");
+        $r1->setAccessible(true);
+        $r2->setAccessible(true);
+        $r1->setValue($instance, CustomiesBravo::$simpleCoreToNetMapping);
+        $r2->setValue($instance, CustomiesBravo::$simpleNetToCoreMapping);
 	}
 
     public function onPacketReceve(DataPacketSendEvent $event){
-	    $packet = $event->getPacket();
-	    if ($packet instanceof StartGamePacket){
-            $old = json_decode(file_get_contents(RESOURCE_PATH . '/vanilla/item_id_map.json'), true);
-            $add = json_decode(file_get_contents(Server::getInstance()->getDataPath()."plugin_data/Unique/config.json"), true);
-            $packet->itemTable =  array_merge($old, $add);
+        $packet = $event->getPacket();
+        if ($packet instanceof StartGamePacket){
+            $packet->itemTable = CustomiesBravo::$entries;
         }
     }
 }
